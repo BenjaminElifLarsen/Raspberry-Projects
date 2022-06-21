c8e4c50df659909351b451a5741af9d17f78629a
Beela <bla@senia.dk>
2022-06-21 14:08:57 +0200

    code bugged right now, yet not bugged

--- case/Case/Case/Case.ino
+++ case/Case/Case/Case.ino
@@ -15,7 +15,13 @@
 static volatile MachineStates State = IDLE;
 static volatile MachineStateStruct WorkingStruct;
 
-#define TEMP_PIN 15;
+int tempPin = 15;
+ 
+#define ECHO_PORT "2E"
+#define ECHO_BIT 4 //bit //PE4 // D2
+int ECHO = 2; //only here because of PulseIn function call
+#define TRIG_PORT "2E" 
+#define TRIG_BIT 3 //bit //PE5 // D3
 
 static volatile bool EndOfStateMachine = false;
 
@@ -29,9 +35,18 @@ void IdleState(void)
 	State = IDLE;
 }
 
-double ReadHCSR04(void)
+int ReadHCSR04(void)
 {
-
+	//digitalWrite(TRIG, 0); //convert to c 
+	PortManipulation(TRIG_PORT, TRIG_BIT, 0);
+	delayMicroseconds(2);
+	//digitalWrite(TRIG, 1);
+	PortManipulation(TRIG_PORT, TRIG_BIT, 1);
+	delayMicroseconds(10);
+	//digitalWrite(TRIG, 0);
+	PortManipulation(TRIG_PORT, TRIG_BIT, 0);
+	long duration = pulseIn(ECHO, 1);
+	return duration * 0.034 / 2;
 }
 
 void DistanceHandling(void)
@@ -41,7 +56,7 @@ void DistanceHandling(void)
 
 float ReadTemperature(void) 
 {
-	int val = analogRead(TEMP_PIN);
+	int val = analogRead(tempPin);
 	float mv = (val / 1024.0) * 5000;
 	return mv / 10;
 }
@@ -74,12 +89,41 @@ MachineStateStruct* StateAllocateMemoryInRamAndGetCopyFromFlashProm()
 	memcpy_FlashProm((char*)MachineStructInRAM_Pointer, (const char*)&(MachineStateStructArray[(uint8_t)State]), sizeof(MachineStateStruct));
 }
 
+void PortManipulation(char* address_string, uint8_t position, uint8_t value)
+{
+	uint8_t* address_Pointer;
+
+	address_Pointer = (uint8_t*)(uint16_t)strtoul((const char*)address_string, NULL, 16);
+
+	if (0 == value)
+	{
+		uint8_t* ddr = address_Pointer;
+		ddr--;
+		*ddr &= ~(1 << position);
+		*address_Pointer &= ~(1 << position);
+	}
+	else
+	{
+		uint8_t* ddr = address_Pointer;
+		ddr--;
+		*ddr |= 1 << position;
+		*address_Pointer |= 1 << position;
+	}
+}
+
 void setup() {
 	USE_SERIAL.begin(9600);
 	debug_init();
 	delay(3000);
+	PortManipulation("25", 3, 1);
+	//pinMode(TRIG, OUTPUT); //move over to C code later
+	PortManipulation(ECHO_PORT, ECHO_BIT, 1);
+	//pinMode(ECHO, INPUT);
 }
 
 void loop() {
-	USE_SERIAL.print("\nTest");
+
+	//USE_SERIAL.println(ReadTemperature());
+	USE_SERIAL.println(ReadHCSR04());
+	delay(1000);
 }
