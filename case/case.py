d2d196d66506c9c24ce310dc0e5a4a848bc9aa86
Beela <bla@senia.dk>
2022-06-23 10:31:05 +0200

    cleaned up

--- case/case.py
+++ case/case.py
@@ -13,10 +13,6 @@ def on_connect(client, userdata, flags, rc, properties=None):
 def on_publish(client, userdata, mid, properties=None):
     print("mid: " + str(mid))
 
-# print which topic was subscribed to
-#def on_subscribe(client, userdata, mid, granted_qos, properties=None):
-#    print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
 # print message, useful for checking if it was successful
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
@@ -24,35 +20,26 @@ def on_message(client, userdata, msg):
 client = paho.Client(client_id="Raspberry Pi", userdata=None, protocol=paho.MQTTv5)
 client.on_connect = on_connect
 
-#client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 
-
-#client.connect("localhost", 1883)
-
-#client.on_subscribe = on_subscribe
 client.on_message = on_message
 client.on_publish = on_publish
 
-#client.subscribe("nej", qos=1)
 
-#client.publish("nej", payload="Yep, nej is neat", qos=1)
 
 
 device = XBeeDevice("/dev/ttyUSB0", 9600)
 
 device.open()
-#take any data from the XBee and transmit to the mqtt
 while(True): 
     message = device.read_data()
-    if message != None: #check if the message is from a trusted MAC.
+    if message != None: 
         data = message.data
         remote = message.remote_device.get_64bit_addr().address.hex()
         print("Remote: ")
         print(remote)
-        print(type(remote))
         if remote == "0013a2004106f9fc":
             mes = data.decode()
-            print("Got data: ")
+            print("PERMITTED")
             print(mes)
             client.connect("localhost",1883)
             client.publish("temp", payload=mes, qos=1)
